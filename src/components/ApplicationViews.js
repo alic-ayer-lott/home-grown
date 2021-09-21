e1679c459559421ea4ebddad4226469d16f22e96
Alicia AyersLott <amayers92@gmail.com>
2021-09-21 15:25:44 -0500

    planner functionality added

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { MyGarden } from "./gardens/MyGarden"
-import { HomePage } from "./home/HomePage"
+// import { HomePage } from "./home/HomePage"
 import { ChatBoard } from "./chat/ChatBoard"
 import { ChatForm } from "./chat/ChatForm"
+import { Planner } from "./planner/Planner"
 
 
 export const ApplicationViews = () => {
@@ -12,8 +13,11 @@ export const ApplicationViews = () => {
             <Route path="/mygarden">
                 <MyGarden />
             </Route>
+            <Route path="/planner">
+                <Planner />
+            </Route>
             <Route exact path="/">
-                <HomePage />
+                <MyGarden />
             </Route>
             <Route exact path="/chat">
                 <ChatBoard />
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -5,11 +5,14 @@ import "./NavBar.css"
 export const NavBar = () => {
     return (
         <ul className="navbar">
-            <li className="navbar__item active">
+            {/* <li className="navbar__item active">
                 <Link className="navbar__link" to="/">Home</Link>
-            </li>
+            </li> */}
             <li className="navbar__item active">
                 <Link className="navbar__link" to="/mygarden">My Garden</Link>
+            </li>
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/planner">Planner</Link>
             </li>
             <li className="navbar__item active">
                 <Link className="navbar__link" to="/chat">Tips From Friends</Link>
